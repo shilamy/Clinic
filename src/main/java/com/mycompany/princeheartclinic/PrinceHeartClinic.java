71966b838f6cd7c7e446bcc0ed960e9723fe92cc
shilamy <macmynev@gmail.com>
2024-06-11 09:29:47 +0300

    An updated version of the previous project two added forms riskmanagement and referral management.

---
+++ src/main/java/com/mycompany/princeheartclinic/PrinceHeartClinic.java
@@ -0,0 +1,16 @@
+
+package com.mycompany.princeheartclinic;
+
+
+public class PrinceHeartClinic {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("Welcome!!!!");
+    }
+    
+}
